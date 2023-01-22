0a0a7898c27baee0c4ea9a3f967ecfd258c4df0a
amShuri <juli.meep93@gmail.com>
2023-01-22 14:01:06 -0300

    Add func to operators that perform the calculation

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
+let calcValues = [];
+let calcOperator = '';
+let calcResult = '';
+
 const numberBtn = document.querySelectorAll('.number-btn');
 const operatorBtn = document.querySelectorAll('.operator-btn');
 const input = document.querySelector('.calculator__operation-input');
+const result = document.querySelector('.calculator__operation-result')
+const calcPattern = /\d+/;
+const operatorPattern = /[\+\-\/\*]/;
 
 numberBtn.forEach(function(button) {
     button.addEventListener('click', () => {
@@ -10,9 +17,32 @@ numberBtn.forEach(function(button) {
 
 operatorBtn.forEach(function(button) {
     button.addEventListener('click', () => {
-        input.value += button.value;
+        if(calcOperator === '') {
+            calcOperator = button.value;
+        }
+        getResults();
+        if(result.textContent.match(operatorPattern)) {
+            result.textContent = result.textContent.slice(0,-1) + button.value;
+            calcOperator = button.value;
+        }
     });
 });
+
+function getResults() {
+    if(calcValues.length < 2 && input.value.match(calcPattern)) {
+        calcValues.push(+input.value.match(calcPattern));
+        result.textContent = `${input.value.match(calcPattern)} ${calcOperator}`;
+        input.value = '';
+    }
+
+    if(calcValues.length === 2) {
+        operate(calcOperator);
+        result.textContent = `${calcResult} ${calcOperator}`;
+        calcValues.splice(0,2,calcResult);
+        input.value = '';
+        calcOperator = '';
+    }
+}
 
 function add(...arg) {
     return arg.reduce((total, curr) => total + curr);
@@ -30,19 +60,19 @@ function divide(...arg) {
     return arg.reduce((total, curr) => total / curr);
 }
 
-function operate(operator, ...calcValues) {
+function operate(operator) {
     switch(operator) {
         case '+':
-            console.log(add(...calcValues));
+            calcResult = add(...calcValues);
             break;
         case '-':
-            console.log(subtract(...calcValues));
+            calcResult = subtract(...calcValues);
             break;
         case '*':
-            console.log(multiply(...calcValues));
+            calcResult = multiply(...calcValues);
             break;
         case '/':
-            console.log(divide(...calcValues));
+            calcResult = divide(...calcValues);
             break;
     }
 }
